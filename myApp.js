ec876483710c94f78d2e82b5c887a72fd03a2e9e
wakivahmbvwinji-web <wakivahmbvwinji@gmail.com>
2025-12-28 21:16:44 +0200

    Refactor myApp.js to improve security and cleanup
    Removed unnecessary code and added helmet middleware to enhance security by hiding the 'X-Powered-By' header.

--- myApp.js
+++ myApp.js
@@ -1,61 +1,9 @@
 const express = require('express');
 const app = express();
 
+const helmet = require('helmet'); // Already required
 
+// Hide "X-Powered-By: Express" header
+app.use(helmet.hidePoweredBy());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
+// FCC testing routes go below
